ff2b0667960fb39c8f12bf03f88d3319ac09e7e6
Shourya <shouryagaur101@gmail.com>
2026-03-28 00:14:38 +0530

    insert in stack at any index

--- Java with DSA/Day 16/Recursion_1toN.java
+++ Java with DSA/Day 16/Recursion_1toN.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Recursion_1toN {
 
-    static void print1toN(int n) {  // prints 1, 2, 3, ..., n
+    static void print1toN(int n) { // prints 1, 2, 3, ..., n
         if (n == 1) {
             System.out.println(1);
             return;
         }
-        print1toN(n - 1);  // first print 1 to n-1
-        System.out.println(n);  // then print n
+        print1toN(n - 1); // first print 1 to n-1
+        System.out.println(n); // then print n
     }
 
     public static void main(String[] args) {
@@ -16,5 +16,6 @@ public class Recursion_1toN {
         System.out.print("Enter value of n: ");
         int n = sc.nextInt();
         print1toN(n);
+        sc.close();
     }
 }
--- Java with DSA/Day 42/moveStackInSameOrder.java
+++ Java with DSA/Day 42/moveStackInSameOrder.java
@@ -6,16 +6,27 @@ public class moveStackInSameOrder {
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
         Stack<Integer> st = new Stack<>();
-        int n;
-        System.out.println("Enter the number elements you want to insert");
-        for(int i=1; i<=n; i++){
-            int x = sc.nextInt();
-            st.push(x);
-        }
+        st.push(1);
+        st.push(2);
+        st.push(3); 
+        st.push(4);
+        st.push(5);
         System.out.println(st);
 
+        //reverse order
         Stack<Integer> rt = new Stack<>();
-        
+        while(st.size() > 0){
+            int x = st.peek();
+            rt.push(x);
+            st.pop();
         }
+
+        Stack<Integer> gt = new Stack<>();
+        while(rt.size() > 0){
+            int x = rt.peek();
+            gt.push(x);
+            rt.pop();
+        }
+        System.out.println(gt);
     }
 }
