9646679ebbd736d34a8c81bc12f47ec231850a81
Shourya <shouryagaur101@gmail.com>
2026-06-08 23:35:25 +0530

    Setup a post feed backend

---
+++ Backend/Postapp/src/db/db.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+    await mongoose.connect('mongodb+srv://api599650_db_user:firstbackend@firstbackend.cbte06o.mongodb.net/Project-Postapp')    
+}
