b6e9f65ba10e9393a48d7226e026e15571cbe896
Shourya <shouryagaur101@gmail.com>
2026-06-09 23:54:30 +0530

    setup and connect db

--- Backend/Postapp/src/db/db.js
+++ Backend/Postapp/src/db/db.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-    await mongoose.connect('mongodb+srv://api599650_db_user:firstbackend@firstbackend.cbte06o.mongodb.net/Project-Postapp')    
+    await mongoose.connect('mongodb+srv://api599650_db_user:firstbackend@firstbackend.cbte06o.mongodb.net/Project-Postapp') 
+    
+    console.log("Connected to DB")
 }
+
+module.exports = connectDB;
