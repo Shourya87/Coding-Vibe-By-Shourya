0e44979a6778f0b9c768c718095b37ab3e2b7e05
Shourya <shouryagaur101@gmail.com>
2026-06-10 23:27:18 +0530

    create services file

--- Backend/Postapp/src/db/db.js
+++ Backend/Postapp/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-    await mongoose.connect('mongodb+srv://api599650_db_user:firstbackend@firstbackend.cbte06o.mongodb.net/Project-Postapp') 
+    await mongoose.connect('mongodb+srv://api599650_db_user:firstbackend@firstbackend.cbte06o.mongodb.net/PostApp') 
     
     console.log("Connected to DB")
 }
