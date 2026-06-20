74984c19283778e0f3dc22886ddc30cba7b252c7
Shourya <shouryagaur101@gmail.com>
2026-06-20 12:04:39 +0530

    JWT auth, authorization, API debugging and Postman testing

---
+++ Backend/Spotify/src/controllers/music.controller.js
@@ -0,0 +1,90 @@
+const musicModel = require("../models/music.model");
+const jwt = require("jsonwebtoken");
+const { uploadFile } = require("../services/storage.service");
+const albumModel = require("../models/album.model");
+
+async function createMusic(req, res) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.role !== "artist") {
+      console.log(decoded);
+      console.log(decoded.role);
+      return res
+        .status(403)
+        .json({ message: "You don't have access to create an music" });
+    }
+
+    const { title } = req.body;
+    const file = req.file;
+
+    const result = await uploadFile(file.buffer.toString("base64"));
+
+    const music = await musicModel.create({
+      uri: result.url,
+      title,
+      artist: decoded.id,
+    });
+
+    res.status(201).json({
+      message: "Music created successfully",
+      music: {
+        id: music._id,
+        uri: music.uri,
+        title: music.title,
+        artist: music.artist,
+      },
+    });
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+}
+
+async function createAlbum(req, res) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.role !== "artist") {
+      return res
+        .status(403)
+        .json({ message: "You don't have access to create album." });
+    }
+
+    const { title, musics } = req.body;
+
+    const album = await albumModel.create({
+      title,
+      artist: decoded.id,
+      musics,
+    });
+
+    res.status(201).json({
+      message: "Album successfully created.",
+      album: {
+        id: album._id,
+        title: album.title,
+        artist: album.artist,
+        musics: album.musics,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+}
+
+module.exports = { createMusic, createAlbum };
