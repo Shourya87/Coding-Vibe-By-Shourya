7ed27981ced47471d69fce4a316e19d43c158e88
Shourya <shouryagaur101@gmail.com>
2026-06-23 23:53:47 +0530

    connect db

--- Backend/Project/backend/server.js
+++ Backend/Project/backend/server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require("cors");
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+const connectDB = require('./src/config/db');
+
+
 
 dotenv.config();
-
-
+connectDB();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 
 
