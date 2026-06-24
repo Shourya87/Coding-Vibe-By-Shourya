44e54f3d08311f7ca06fc5bdc243444b78155778
Shourya <shouryagaur101@gmail.com>
2026-06-24 23:44:36 +0530

    make user schema, auth route

--- Backend/Project/backend/server.js
+++ Backend/Project/backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./src/config/db');
+const authRoutes = require('./src/routes/auth.routes')
 
 
 
@@ -19,6 +20,14 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.send("Backend Working.");
 })
+
+
+app.use('api/auth', authRoutes)
+
+
+
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>{
---
+++ Backend/Project/backend/src/routes/auth.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser, logoutUser } = require('../controllers/auth.controller')
+
+
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/user', logoutUser);
