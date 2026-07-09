079e07118ff9f7e814a764eeeab18234977d08d5
Shourya <shouryagaur101@gmail.com>
2026-07-09 18:35:34 +0530

    Learn about Typescript and installation , setup of Ts.

---
+++ TypeScript/test.js
@@ -0,0 +1,5 @@
+"use strict";
+var userName = "Shourya";
+var age = 20;
+var Age = 30;
+console.log(userName, "\n", age);
---
+++ TypeScript/test.ts
@@ -0,0 +1,6 @@
+var userName : string = "Shourya";
+var age : number = 20;
+
+var Age = 30;
+
+console.log(userName,"\n", age);
