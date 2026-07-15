0585ee868cfeb2e7ea5ca25782db1bb2313ce293
Shourya <shouryagaur101@gmail.com>
2026-07-16 00:28:38 +0530

    Learn about tuples. Learn about Object. Learn about Any and unknown data types.

---
+++ TypeScript/Day6.ts
@@ -0,0 +1,61 @@
+// Tuple
+// var emp : string[] = ['anil', 'avni', 'ram'];
+// var empData : [string, number, boolean] = ['anil', 20, true];
+
+// console.log(empData);
+// console.log(typeof emp);
+// console.log(typeof empData);
+
+// empData.push("noida");
+
+// console.log(empData);
+
+
+
+//Object
+// var userData : {
+//     name:string, 
+//     age: number, 
+//     company: string | undefined,
+// } = {
+//     name: "Anil",
+//     age: 30,
+//     company: undefined,
+// };
+
+// console.log(userData);
+// console.log(typeof userData);
+
+// userData.company = "HCL"
+// console.log(userData);
+
+
+// var userData : {
+//     [key:string]: string | number | undefined
+// } = {
+//     name: "Anil",
+//     age: 30,
+//     company: undefined,
+// };
+
+// console.log(userData);
+// console.log(typeof userData);
+
+// userData.company = "HCL";
+// userData.city = "Noida";
+// console.log(userData);
+
+
+
+
+// Any and Unknown
+var value: any = "Anil";
+value = 100
+value = ["anil"];
+ 
+console.log(value);
+console.log(typeof value);
+
+var v2 : unknown = 'riya';
+v2 = 10;
+console.log(typeof v2);
