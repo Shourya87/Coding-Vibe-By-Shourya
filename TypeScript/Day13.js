2079ee5c2600a2e334958cbd258f24c697ff91e6
Shourya <shouryagaur101@gmail.com>
2026-07-25 00:12:16 +0530

    Learn about instanceof

--- TypeScript/Day13.js
+++ TypeScript/Day13.js
@@ -11,3 +11,33 @@ var Company = /** @class */ (function () {
 var t1 = new Company();
 console.log(Company.name);
 console.log(t1.getName());
+// Type Guard
+var ud = "Nil";
+console.log(ud);
+if (typeof ud == "boolean") {
+    console.log('This is a boolean');
+}
+else if (typeof ud == "string") {
+    console.log('This is a string');
+}
+var pro2 = /** @class */ (function () {
+    function pro2() {
+    }
+    return pro2;
+}());
+var p1 = new pro2();
+var or2 = /** @class */ (function () {
+    function or2() {
+    }
+    return or2;
+}());
+var o1 = new or2();
+function checkData(data) {
+    if (data instanceof or2) {
+        console.log("This is a order");
+    }
+    else {
+        console.log("This is a product");
+    }
+}
+checkData(o1);
--- TypeScript/Day13.ts
+++ TypeScript/Day13.ts
@@ -9,3 +9,38 @@ class Company{
 var t1 = new Company();
 console.log(Company.name)
 console.log(t1.getName());
+
+
+// Type Guard
+let ud: number | string | boolean = "Nil";
+console.log(ud);
+
+if(typeof ud == "boolean") {
+    console.log('This is a boolean');
+}
+
+else if(typeof ud == "string") {
+    console.log('This is a string');
+}
+
+
+class pro2 {
+
+}
+
+var p1 = new pro2();
+class or2 {
+
+}
+
+var o1 = new or2();
+
+function checkData (data: or2 | pro2) {
+    if (data instanceof or2) {
+        console.log("This is a order");
+    } else {
+        console.log("This is a product");
+    }
+}
+
+checkData(o1);
